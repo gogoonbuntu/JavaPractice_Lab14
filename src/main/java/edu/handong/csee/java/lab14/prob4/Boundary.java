e91ac93cb1d52cbb5280c6ce9d41bd3e6eebae0a
GGBT <tmddud333@naver.com>
2018-05-11 05:01:15 +0900

    prob4 comments ok

--- src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Boundary.java
@@ -2,16 +2,17 @@ package edu.handong.csee.java.lab14.prob4;
 import java.util.*;
 public class Boundary {
 	private int a[] = new int[5];
+	/* Rcv stands for receive, it may bring Array Index Out of Boundary Exception*/
 	public void Rcv() throws ArrayIndexOutOfBoundsException
 	{
-		int i=0;
+		int i=0; //for index of array
 		Scanner sc = new Scanner(System.in);
-		while(true)
+		while(true) //a loop that infinitely receive input until no error
 		{
-			System.out.print("Enter an integer: ");
-			a[i] = sc.nextInt();
-			System.out.println("arr["+i+"] <- "+a[i]);
-			i++;
+			System.out.print("Enter an integer: "); //get an integer and
+			a[i] = sc.nextInt(); //add to the array
+			System.out.println("arr["+i+"] <- "+a[i]); //indicate the addition is complete.
+			i++; //get to next index.
 		}
 	}
 }
--- src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob4/Main.java
@@ -4,14 +4,15 @@ public class Main {
 
 	public static void main(String[] args) 
 	{
-		Boundary abc = new Boundary();
+		Boundary abc = new Boundary(); //initiate a new object.
 		try 
 		{
-			abc.Rcv();
+			abc.Rcv(); //receive numbers to array.
 		}
 		catch (ArrayIndexOutOfBoundsException e)
 		{
 			System.out.println("Invalid array index access!");
+			//prints out the error message.
 		}
 	}
 }
