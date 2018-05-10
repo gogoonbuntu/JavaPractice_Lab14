cfdd7fd42f5bf24d7d0bc7ed57104391af4b2dc8
GGBT <tmddud333@naver.com>
2018-05-11 05:07:51 +0900

    prob3 comments ok

--- src/main/java/edu/handong/csee/java/lab14/prob3/Main.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Main.java
@@ -3,19 +3,21 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		Powercalc my_calculator = new Powercalc();
+		Powercalc my_calculator = new Powercalc();//initiate a new object.
 		Scanner sc = new Scanner(System.in);
-		int n, p;
+		int n, p; //stands for number, power
 		while(true)
 		{
 			try 
 			{
-				n=sc.nextInt();p=sc.nextInt();
+				n=sc.nextInt();p=sc.nextInt();//receive integers.
 				System.out.println(my_calculator.power(n,p));
+				//print out the result!
 			}
 			catch (Exception e)
 			{
 				System.out.println(e);
+				//print out the error message.
 			}
 		}
 	}
--- src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
+++ src/main/java/edu/handong/csee/java/lab14/prob3/Powercalc.java
@@ -6,12 +6,15 @@ public class Powercalc {
 		if(n==0&&p==0)
 		{
 			throw new Exception ("n and p should not be zero.");
+			//number AND the power cannot be 0 at the same time.
 		}
 		else if(n<0||p<0)
 		{
-			throw new Exception ("n or p should not be negative."); 
+			throw new Exception ("n or p should not be negative.");
+			//number OR the power cannot be negative numbers. 
 		}
 		long res = (long) Math.pow(n, p);
+		//the power calculation, since the result could be big, use long type.
 		return res;
 	}
 }
